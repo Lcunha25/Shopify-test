f8263b6e69c4f76a26a8ac0e16359b7800a210fd
Lucas <lucaspereiracunha08@gmail.com>
2018-05-10 11:42:18 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shopify-test
+## This is a test for a sign-up page on Shopify using SCSS, HTML5 and JavaScript.
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const gulp = require('gulp');
+const prettyError = require('gulp-prettyerror');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+const eslint = require('gulp-eslint');
+const browserSync = require('browser-sync');
+
+// Create basic Gulp tasks
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/style.scss', { sourcemaps: true })
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(gulp.dest('./'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('lint', function() {
+  return gulp
+    .src(['./js/*.js'])
+    .pipe(prettyError())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task(
+  'scripts',
+  gulp.series('lint', function() {
+    return gulp
+      .src('./js/*.js')
+      .pipe(uglify())
+      .pipe(
+        rename({
+          extname: '.min.js'
+        })
+      )
+      .pipe(gulp.dest('./build/js'));
+  })
+);
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: './'
+      }
+  });
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', gulp.series('scripts'));
+  gulp.watch('sass/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch', 'sass', 'lint'));
